e5c95610fd05372ebe831f42a446ceeb57f267ae
Agit config --global user.name soom4478git config --global user.email s2305@e-mirim.hs.kr <s2305@e-mirim.hs.kr>
2025-03-21 08:57:40 +0900

    post 요청

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 // express 애플리케이션을 생성합니다.
 const app = express();
 
-// 루트 경로('/')에 대한 GET 요청을 처리합니다.
-app.get('/', (req, res) => {
+app.get('/s', (req, res) => {
   // 응답 본문에 'Hello, World!'를 보냅니다.
-  res.send('Hello, World!');
+  res.send('get s');
+});
+
+app.post('/s', (req, res) => {
+  // 응답 본문에 'Hello, World!'를 보냅니다.
+  res.send('post s');
 });
 
 // 서버가 포트 3000에서 요청을 대기합니다.
