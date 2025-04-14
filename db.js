b084ad3a0f4d6f146c4b1d7232dc7c5a1efc40f9
Agit config --global user.name soom4478git config --global user.email s2305@e-mirim.hs.kr <s2305@e-mirim.hs.kr>
2025-04-14 11:09:45 +0900

    createPool로 대체

--- db.js
+++ db.js
@@ -3,19 +3,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-});
-
-db.connect(err => {
-    if(err) {
-        console.error('MySQL 연결 실패:', err);
-        return;
-    }
-    console.log('MySQL에 연결되었습니다.');
 });
 
 module.exports = db;
