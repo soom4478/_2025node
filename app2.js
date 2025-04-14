5db170902ae110ae49b1dc8ad536d8ea911c2dfd
Agit config --global user.name soom4478git config --global user.email s2305@e-mirim.hs.kr <s2305@e-mirim.hs.kr>
2025-04-14 10:53:07 +0900

    Update 프론트, Delete

--- app2.js
+++ app2.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const methodOverride = require('method-override');
 
 const port = 3000;
 
@@ -15,6 +16,8 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+app.use(methodOverride('_method'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -95,7 +98,42 @@ app.put('/travel/:id', (req, res) => {
         }
         res.render('updateSuccess');
     })
-})
+});
+
+app.delete('/travel/:id', (req, res) => {
+    const travelID = req.params.id;
+    const _query = 'DELETE FROM travellist WHERE id = ?';
+    db.query(_query, [travelID], (err, results) => {
+        if(err) {
+            console.error('DB 쿼리 실패', err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+        if(results.length === 0) {
+            res.status.apply(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        res.render('deleteSuccess');
+    })
+});
+
+app.get('/travel/:id/edit', (req, res) => {
+    const travelID = req.params.id;
+    const _query = 'SELECT * FROM travellist WHERE id = ?';
+    db.query(_query, [travelID], (err, results) => {
+        if(err) {
+            console.error('DB 쿼리 실패', err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+        if(results.length === 0) {
+            res.status.apply(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        const travel = results[0];
+        res.render('editTravel', {travel});
+    })
+});
 
 app.get('/add-travel', (req, res) => {
     res.render('addTravel');
