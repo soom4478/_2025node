c9b6db2796858588c24f604820a77648d9a5ee58
Agit config --global user.name soom4478git config --global user.email s2305@e-mirim.hs.kr <s2305@e-mirim.hs.kr>
2025-04-14 11:07:18 +0900

    라우팅 순서 정리

--- routes/travel.js
+++ routes/travel.js
@@ -3,10 +3,7 @@ const db = require('../db');
 
 const router = express.Router();
 
-router.get('/add', (req, res) => {
-    res.render('addTravel');
-});
-
+// 게시글 목록을 보여줌
 router.get('/', (req, res) => {
     const _query = 'SELECT id, name FROM travellist';
     db.query(_query, (err, results) => {
@@ -20,6 +17,26 @@ router.get('/', (req, res) => {
     });
 });
 
+// 게시글을 추가하는 페이지를 보여줌
+router.get('/add', (req, res) => {
+    res.render('addTravel');
+});
+
+// 게시글을 추가
+router.post('/', (req, res) => {
+    const {name} = req.body; // body안 name 속성만 가져옴. const name = req.body.name과 동일
+    const _query = 'INSERT INTO travellist (name) VALUES (?)';
+    db.query(_query, [name], (err, results) => {
+        if(err) {
+            console.error('데이터베이스 쿼리 실패');
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.redirect('/travel');
+    });
+});
+
+// 게시글 내용 확인
 router.get('/:id', (req, res) => {
     const travelID = req.params.id;
     const _query = 'SELECT * FROM travellist WHERE id = ?';
@@ -38,19 +55,26 @@ router.get('/:id', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
-    const {name} = req.body; // body안 name 속성만 가져옴. const name = req.body.name과 동일
-    const _query = 'INSERT INTO travellist (name) VALUES (?)';
-    db.query(_query, [name], (err, results) => {
+// 게시글을 수정하는 페이지를 보여줌
+router.get('/:id/edit', (req, res) => {
+    const travelID = req.params.id;
+    const _query = 'SELECT * FROM travellist WHERE id = ?';
+    db.query(_query, [travelID], (err, results) => {
         if(err) {
-            console.error('데이터베이스 쿼리 실패');
-            res.status(500).send('Internal Server Error');
+            console.error('DB 쿼리 실패', err);
+            res.status(500).send('내부 서버 에러');
             return;
         }
-        res.redirect('/travel');
-    });
+        if(results.length === 0) {
+            res.status.apply(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        const travel = results[0];
+        res.render('editTravel', {travel});
+    })
 });
 
+// 게시글을 수정
 router.put('/:id', (req, res) => {
     const travelID = req.params.id;
     const {name} = req.body;
@@ -69,6 +93,7 @@ router.put('/:id', (req, res) => {
     })
 });
 
+// 게시글을 삭제
 router.delete('/:id', (req, res) => {
     const travelID = req.params.id;
     const _query = 'DELETE FROM travellist WHERE id = ?';
@@ -83,24 +108,6 @@ router.delete('/:id', (req, res) => {
             return;
         }
         res.render('deleteSuccess');
-    })
-});
-
-router.get('/:id/edit', (req, res) => {
-    const travelID = req.params.id;
-    const _query = 'SELECT * FROM travellist WHERE id = ?';
-    db.query(_query, [travelID], (err, results) => {
-        if(err) {
-            console.error('DB 쿼리 실패', err);
-            res.status(500).send('내부 서버 에러');
-            return;
-        }
-        if(results.length === 0) {
-            res.status.apply(404).send('여행지를 찾을 수 없습니다.');
-            return;
-        }
-        const travel = results[0];
-        res.render('editTravel', {travel});
     })
 });
 
